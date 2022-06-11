b12b3d0e09de3e8f422043b906001e3cf833d91e
Yoris95 <98592375+Yoris95@users.noreply.github.com>
2022-06-12 00:29:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-document.querySelector('#push').onclick = function
-(){
-    if(document.querySelector('#newtask input').value.length == 0){
-        alert("Please Enter a Task")
-    }
-    else{
-        document.querySelector('#task').innerHTML
-        +=`
+document.querySelector("#push").onclick = function () {
+  if (document.querySelector("#newtask input").value.length == 0) {
+    alert("Please Enter a Task");
+  } else {
+    document.querySelector("#task").innerHTML += `
             <div class="task">
                 <span id="taskname">
-                    ${document.querySelector('#newtask input').value}
+                    ${document.querySelector("#newtask input").value}
                 </span>
                 <button class="delete">
                     <i class="far fa-trash-alt"></i>
@@ -16,19 +13,20 @@ document.querySelector('#push').onclick = function
             </div>      
         `;
 
-        var current_tasks = document.querySelectorAll(".delete");
-            for (var i=0; i<current_tasks.length; i++){current_tasks[i].onclick = function(){
-                this.parentNode.remove();
-            }
-        }
-        
-        var task = document.querySelectorAll(".task");
-            for (var i=0; i<task.length; i++){task[i].onclick=function(){
-            this.classList.toggle('completed');
-        }
+    var current_tasks = document.querySelectorAll(".delete");
+    for (var i = 0; i < current_tasks.length; i++) {
+      current_tasks[i].onclick = function () {
+        this.parentNode.remove();
+      };
     }
-       
-        document.querySelector("#newtask input").value = "";
 
-    }   
-}
+    var task = document.querySelectorAll(".task");
+    for (var i = 0; i < task.length; i++) {
+      task[i].onclick = function () {
+        this.classList.toggle("completed");
+      };
+    }
+
+    document.querySelector("#newtask input").value = "";
+  }
+};
